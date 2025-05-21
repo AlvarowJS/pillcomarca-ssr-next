56cc365945a3bc3944d5d59ea5ee88492945ec02
PierZ7 <alexandergarciapaitan@gmail.com>
2025-05-21 12:32:40 -0500

    Desarrollo de las paginas Organigrama, Acceso a la informacion y FUT

--- src/app/(landing)/organigrama/page.tsx
+++ src/app/(landing)/organigrama/page.tsx
@@ -13,8 +13,47 @@ export const metadata: Metadata = {
 
 export default function OrganigramaPage() {
   return (
-    <div>
-      <h1>Organigrama</h1>
+        <div className="p-4 md:p-6 lg:p-8">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-8 items-start">
+        
+        <div className="col-span-1 text-left md:text-left">
+          <h2 className="text-xl md:text-2xl font-bold text-cyan-500 mb-4">
+            Organigrama Institucional
+          </h2>
+          <p >
+            El organigrama vigente es de 2013, el cual se encuentra en el ROF y lo puede encontrar en los
+            <a href="/documentos" className="text-blue-500 underline ml-1">documentos de gestión</a> de la página.
+          </p>
+          <p >
+            Este documento permite visualizar la estructura jerárquica de la Municipalidad, incluyendo las áreas y funciones específicas asignadas a cada unidad.
+          </p>
+          <br />
+            <a
+              href="/documents/Organigrama.pdf"
+              download
+              className="inline-flex items-center gap-2 px-4 py-2 bg-cyan-500 text-white font-semibold rounded hover:bg-cyan-600 transition"
+            >
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              aria-hidden="true"
+            >
+              <path d="M5 20h14v-2H5v2zM19 9h-4V3H9v6H5l7 7 7-7z" />
+            </svg>
+              Descargar
+            </a>
+        </div>
+
+        <div className="col-span-1 md:col-span-3">
+          <iframe
+            src="/documents/Organigrama.pdf"
+            className="w-full h-[500px] rounded border border-gray-500 shadow"
+            title="Organigrama"
+          ></iframe>
+        </div>
+
+      </div>
     </div>
   );
 }
